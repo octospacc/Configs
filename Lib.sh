dbdcde20eb1c9e1a9e5561506084cf6d3c910260
octospacc <octo@tutamail.com>
2023-04-14 18:47:33 +0200

    Upd

---
+++ Lib.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+mkcd(){
+	mkdir -vp "$1" && \
+	cd "$1"
+}
--- Server/Repo.Update.sh
+++ Server/Repo.Update.sh
@@ -1,17 +1,34 @@
 #!/bin/sh
 cd "$( dirname "$( realpath "$0" )" )"
-cd ./Root
+. ../Lib.sh
 
-cp -v /etc/diycron ./etc/diycron
+h=home/pi
 
-cp -v \
-	/etc/systemd/system/diycron.service \
-	./etc/systemd/system/
+mkcd ./Root
+	for p in \
+		diycron
+	do
+		cp -v /etc/$p ./etc/$p
+	done
 
-cp -v \
-	/etc/nginx/sites-available/*.conf /etc/nginx/sites-available/*.old \
-	./etc/nginx/sites-available/
+	cp -v \
+		/etc/systemd/system/diycron.service \
+		./etc/systemd/system/
 
-cp -v \
-	/Server/Scripts/Backup/*.sh /Server/Scripts/Backup/*.cfg \
-	./Server/Scripts/Backup/
+	cp -v \
+		/etc/nginx/sites-available/*.conf /etc/nginx/sites-available/*.old \
+		./etc/nginx/sites-available/
+
+	cp -v \
+		/Server/Scripts/Backup/*.sh /Server/Scripts/Backup/*.cfg \
+		./Server/Scripts/Backup/
+
+cd ..
+
+mkcd ./Home
+	mkdir -vp ./.config
+	for p in \
+		aria2
+	do
+		cp -vrT /$h/.config/$p ./.config/$p
+	done
