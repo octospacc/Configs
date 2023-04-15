f7f581ddb699c5df5e87732a8f1c3490528945b0
octospacc <octo@tutamail.com>
2023-04-16 00:29:42 +0200

    Upd bittorrent

--- Server/Repo.Update.sh
+++ Server/Repo.Update.sh
@@ -5,23 +5,30 @@ cd "$( dirname "$( realpath "$0" )" )"
 h=home/pi
 
 mkcd ./Root
+	mkdir -vp ./etc
 	for p in \
 		diycron
 	do
 		cp -v /etc/$p ./etc/$p
 	done
 
+	mkdir -vp ./etc/systemd/system
 	cp -v \
 		/etc/systemd/system/diycron.service \
 		./etc/systemd/system/
 
+	mkdir -vp ./etc/nginx/sites-available
 	cp -v \
 		/etc/nginx/sites-available/*.conf /etc/nginx/sites-available/*.old \
 		./etc/nginx/sites-available/
 
+	mkdir -vp ./Server/Scripts/Backup
 	cp -v \
 		/Server/Scripts/Backup/*.sh /Server/Scripts/Backup/*.cfg \
 		./Server/Scripts/Backup/
+
+	mkdir -vp ./Server/Start
+	cp -v /Server/Start/bittorrentd ./Server/Start/
 
 	cpdir Transfers/aria2/Conf
 
