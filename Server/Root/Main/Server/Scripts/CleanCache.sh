5ff8dedf6f595f50c923baee9ec4a2b3e82d2d6f
octospacc <octo@tutamail.com>
2025-05-27 02:24:23 +0200

    Auto-Backup $'Tue May 27 2025 01:42:07 GMT+0200 (Central European Summer Time)'

--- Server/Root/Main/Server/Scripts/CleanCache.sh
+++ Server/Root/Main/Server/Scripts/CleanCache.sh
@@ -5,6 +5,15 @@ if [ "$(id -u)" != 0 ]; then
 	exit 1
 fi
 
+apt autoremove --purge
+lxc-attach Debian2023 -- apt autoremove --purge
+
+rm -rf /Main/Backup/*.old
+
+rm -f /Main/Server/www/pixelfed_liminalgici/storage/logs/*.log
+
+sh -c 'truncate -s 100M /var/lib/docker/containers/*/*-json.log'
+
 docker exec -it pihole rm -f /var/log/pihole/pihole.log
 docker restart pihole
 
